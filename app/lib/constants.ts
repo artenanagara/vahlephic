b8a4a1fedfa4fbe1a528fe9cd4315eaadc9aa7af
Artena Nagara <artenanagara22@gmail.com>
2026-06-17 12:21:59 +0700

    Update CV file link

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -16,4 +16,4 @@ export const ASSETS = {
 } as const;
 
 export const CV_URL =
-  "https://drive.google.com/drive/u/1/folders/18oe8hXgYVU5mu_Am96ahAOqIeXuJoL_f";
+  "https://drive.google.com/file/d/1YTvWyifmSqNGcTFAkgLydPtdpSB-JwFV/view?usp=drive_link";
